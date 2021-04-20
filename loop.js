1f5776d532b2459ca7fb2e1d48f144770666c5a5
laurennrmoore <lmooreintech2021@gmail.com>
2021-04-19 23:05:12 -0400

    loophwpush

---
+++ loop.js
@@ -0,0 +1,49 @@
+// Create a variable that is equal to an empty array and push the values 1-10 into the array using a For Loop
+
+var  myArray  = [];
+ for  (var i= 1; i < 11; i++){
+myArray.push (i);
+}  console.log(myArray);
+
+
+
+
+// Repeat the same steps as above, but you're going to use a While Loop instead, push the values 1-10(inclusive) in descending order.
+
+var myArray = [];
+while (var i = 10; i > 0 ;  i -= 1) {
+myArray.push (i);
+} console.log(myArray);
+
+
+
+//Again set a Variable to an empty array but this time push a set of odd numbers 11-21 into the array using a For Loop.
+
+var myArray = [];
+For (var i = 21; i <12;  i -= 2 ) {
+myArray.push(i);
+} console.log(myArray);
+
+
+
+
+
+//This time around you'll be pushing values onto an array but you will be counting backwards instead! Same set up as the other three questions, but use the numbers 15-1 and count backwards by 2.
+
+
+var myArray = [];
+for (var i = 1;  i < 16;  i -= 2) {
+myArray.push (i);
+} 
+
+
+
+
+
+
+
+
+
+
+
+
